c9139f847c08e2ffd6387bd362e5430b959fb7fa
Dipsii1 <nadifaryastia@gmail.com>
2026-03-08 12:59:48 +0700

    update in next.config

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from "react";
 import VerifyEmailClient from "./VerifyEmailClient";
 
-export const dynamic = "force-dynamic";
-
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
